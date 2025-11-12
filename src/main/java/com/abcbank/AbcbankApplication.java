8cc6860ca9f74e5715c9151c8fdb626950bd73bb
Ajeetkumar Vishwakarma <ajeetkumarv7@gmail.com>
2025-11-12 17:30:48 +0100

    Adding database connection spring configuration

--- src/main/java/com/abcbank/AbcbankApplication.java
+++ src/main/java/com/abcbank/AbcbankApplication.java
@@ -3,10 +3,7 @@ package com.abcbank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class AbcbankApplication {
 
 	public static void main(String[] args) {
